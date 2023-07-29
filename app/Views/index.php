aad1a1544b6a8da99f055a020a5ef65b81e2282f
vertplay <32456872+vertplay@users.noreply.github.com>
2023-07-28 22:06:57 -0300

    página inicial

--- app/Views/index.php
+++ app/Views/index.php
@@ -6,7 +6,7 @@
 			<div id="index-text">
 				<h1>AraClín: Catálogo de Clínicas Médicas</h1>
 				<h2>Seu guia confiável para cuidar da sua saúde!</h2>
-				<a href="<?=base_url('list')?>">Lista</a>
+				<a href="<?=base_url('list')?>">ENCONTRE UMA CLÍNICA</a>
 			</div>
 		</div>
 		
