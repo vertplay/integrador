ebf70a79afc328766d290b28775e0101e17bbc9c
andradetr18 <Andradetr18@gmail.com>
2023-08-09 22:31:13 -0300

    Create validarsenha.js

---
+++ public/js/validarsenha.js
@@ -0,0 +1,19 @@
+function validarSenhaForte(senha) {
+    // verificar os critérios da senha
+    const temNumero = /[0-9]/.test(senha);
+    const temLetraMaiuscula = /[A-Z]/.test(senha);
+    const temLetraMinuscula = /[a-z]/.test(senha);
+    const temCaractereEspecial = /[!@#$%^&*()_+{}\[\]:;<>,.?~]/.test(senha);
+  
+    // Verificar se a senha atende a todos os critérios
+    const senhaValida =
+      temNumero &&
+      temLetraMaiuscula &&
+      temLetraMinuscula &&
+      temCaractereEspecial &&
+      senha.length >= 8; // A senha precisa ter pelo menos 8 caracteres
+  
+    return senhaValida;
+  }
+  
+  
