044cdbc7ee561804fe48cc7ef9df0deea407ca72
Roberta Vieira <139922545+robertasvieira@users.noreply.github.com>
2023-08-16 15:55:55 -0300

    .

--- app/Views/clinica.php
+++ app/Views/clinica.php
@@ -25,34 +25,28 @@
 				<div id="album-block-img">
 					<img src="<?= base_url('/img/'.$clin["ID_clinica"])?>"/>
 				</div>
-
-				<div class="info"> <!--sem dados no BD -->
-					<h3>Horário de Funcionamento:</h3>
-				<p>Segunda-feira: 07:00 às 20:00.<br>
-					Terça-feira: 07:00 às 20:00.<br>
-					Quarta-feira: 07:00 às 20:00.<br>
-					Quinta-feira: 07:00 às 20:00.<br>
-					Sexta-feira: 07:00 às 20:00.<br>
-					Sábado: 07:00 às 11:00.<br>
-					Domingo: Fechado</p>
-				</div>
+				
 				<div class="info">
+					</br>
 					<h3>Contato:</h3>
 					<p>Telefone: <?=formataTelefone($clin['Telefone_clinica'])?><br>
-					
 					WhatsApp: <a target="_blank" href="https://api.whatsapp.com/send?phone=55<?=$clin['Whatsapp_clinica']?>"><?=formataTelefone($clin['Whatsapp_clinica'])?></a><br>
 					Intagram: <a target="_blank" href="https://www.instagram.com/<?=$clin['Instagram_clinica']?>">@<?=$clin['Instagram_clinica']?></a><br>
 					Email: <?=$clin['Email_clinica']?></p>
 				</div>
+
+				<div class="info">
+				</br>
+				<h3>Endereço:</h3>
+					<p>
+					<?= $clin['Logradouro'] ?>, <?= $clin['Numero'] ?> - <?= $clin['Bairro'] ?>, <?= $clin['Complemento'] ?> - Araçuaí, MG, <?= $clin['Cep'] ?>.
+					</p>
+
+				</div>
+
 			</div>
 
 			<div id="clin-block2" class="clin-block">
-				<div class="info">
-					<h3>Endereço:</h3>
-					<p>
-					Av. das Rosas, 40 - Alto Mercado, Araçuaí - MG, 39600-000</p>
-
-				</div>
 				
 				<div class="info">
 					<h3>Avaliações:</h3>
