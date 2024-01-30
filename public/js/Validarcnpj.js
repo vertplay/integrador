5b864c9be9d6dfb83aa77612745476be102925bc
andradetr18 <Andradetr18@gmail.com>
2024-01-30 00:34:51 -0300

    Update Validarcnpj.js

--- public/js/Validarcnpj.js
+++ public/js/Validarcnpj.js
@@ -1,10 +1,54 @@
 // Função para validar CNPJ
 function validarCNPJ(cnpj) {
-    // Implementação da validação de CNPJ aqui
-    // ...
+   // Remover caracteres não numéricos
+   cnpj = cnpj.replace(/\D/g, '');
 
-    // Retorne true se o CNPJ for válido, false caso contrário
-    return true;
+   // Verificar se o CNPJ tem 14 dígitos
+   if (cnpj.length !== 14) {
+       return false;
+   }
+
+   // Verificar se todos os dígitos são iguais (caso contrário, é considerado inválido)
+   if (/^(\d)\1+$/.test(cnpj)) {
+       return false;
+   }
+
+   // Calcular os dígitos verificadores
+   const tamanho = cnpj.length - 2;
+   const numeros = cnpj.substring(0, tamanho);
+   const digitos = cnpj.substring(tamanho);
+   let soma = 0;
+   let pos = tamanho - 7;
+
+   for (let i = tamanho; i >= 1; i--) {
+       soma += parseInt(numeros.charAt(tamanho - i), 10) * pos--;
+       if (pos < 2) {
+           pos = 9;
+       }
+   }
+
+   const resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+   // Verificar primeiro dígito verificador
+   if (resultado !== parseInt(digitos.charAt(0), 10)) {
+       return false;
+   }
+
+   tamanho++;
+   soma = 0;
+   pos = tamanho - 7;
+
+   for (let i = tamanho; i >= 1; i--) {
+       soma += parseInt(numeros.charAt(tamanho - i), 10) * pos--;
+       if (pos < 2) {
+           pos = 9;
+       }
+   }
+
+   const segundoDigito = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+   // Verificar segundo dígito verificador
+   return segundoDigito === parseInt(digitos.charAt(1), 10);
 }
 
 // Função para mostrar mensagem de erro para o CNPJ
@@ -13,9 +57,6 @@ function mostrarErroCNPJ(message) {
     alertaCnpj.innerHTML = message;
     alertaCnpj.style.color = 'red';
 }
-
-
-// Restante do seu código JavaScript aqui...
 
 
 window.onload = function() {
