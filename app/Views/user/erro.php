c669229199d5b438160575cfe9c1e7170ee48b7d
vertplay <artursluzzi@gmail.com>
2024-01-30 14:28:16 -0300

    .

--- app/Views/user/erro.php
+++ app/Views/user/erro.php
@@ -3,12 +3,12 @@
 <?= $this->section('content'); ?>
 <!-- Exibir as mensagens aqui -->
 <?php if (session()->has('error_message')) : ?>
-    <div class="alert alert-danger">
+    <div class="alert alert-danger formularios">
         <strong></strong> <?= session('error_message') ?>
     </div>
 <?php endif; ?>
 <?php if (session()->has('success_message')) : ?>
-    <div class="alert alert-success">
+    <div class="alert alert-success formularios">
         <strong></strong> <?= session('success_message') ?>
     </div>
 <?php endif; ?>
