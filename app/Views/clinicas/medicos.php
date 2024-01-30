b90345355dca4b8b4f1b2c1386d964a826b5f81d
vertplay <artursluzzi@gmail.com>
2024-01-30 13:45:28 -0300

    .

--- app/Views/clinicas/medicos.php
+++ app/Views/clinicas/medicos.php
@@ -17,7 +17,25 @@
     </div>
 
     <div id="clin-block2" class="clin-block">
+        <div class="update-form formularios" id="update-form">
+			<h2>Cadastrar Médico</h2>
+			<h6></h6>
+			<form id="update-form" action="<?=base_url('??')?>" method="POST" enctype="multipart/form-data">
+            <br/>
+                <label for="nome_medico">Nome do médico:</label>
+                    <input type="text" name="nome_medico" id="nome_medico" required/>
+                    <div class="error-message"></div>
+                <label for="especialidade_medico">Especialidade:</label>
+                    <input type="text" name="especialidade_medico" id="especialidade_medico" required/>
+                    <div class="error-message"></div>
+                <label for="crm_medico">CRM:</label>
+                   <input type="text" name="crm_medico" id="crm_medico" required/>
+                    <div class="error-message"></div>
 
+				<button type="submit" name="cadastrar">Cadastrar</button>
+					<button type="button" id="cancelarUpdate_btn">Cancelar</button>
+			</form>
+		</div>
     </div>
 
 </div>
