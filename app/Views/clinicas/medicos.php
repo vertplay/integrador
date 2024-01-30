5879f8febb493c92f7954539cd221562b96e5069
vertplay <artursluzzi@gmail.com>
2024-01-30 14:01:50 -0300

    front-end médicos

--- app/Views/clinicas/medicos.php
+++ app/Views/clinicas/medicos.php
@@ -2,16 +2,20 @@
 	
 <?= $this->section('content'); ?>
 
-
+<?php $session = session();?>
 <div id="perfil-pag">
     <div id="clin-block1" class="clin-block">
         <?php foreach($dados as $dado){ $dado = $dado[0]; ?>
 
-            <div class="info">
-					<h3><?=$dado['Nome_medico']?></h3>
-					<p>Especialidade: <?=$dado['Especialidade_medico']?></p>
-                    <p>CRM: <?=$dado['CRM_medico']?></p>
-					</div>
+            <div class="info lista_medicos">
+				<h3><?=$dado['Nome_medico']?></h3>
+				<p>Especialidade: <?=$dado['Especialidade_medico']?></p>
+                <p>CRM: <?=$dado['CRM_medico']?></p>
+                <form id="form" action="<?=base_url('??')?>" method="POST" enctype="multipart/form-data">
+					<input type="text" name="ID_medico" id="ID_medico" value="<?=$dado['ID_medico']?>" style="display:none;"/>
+					<button type="submit" id="cancelarUpdate_btn" value="<?=$session->get('ID_clinica')?>">Excluir</button>
+				</form>
+			</div>
 
         <?php } ?>    
     </div>
@@ -33,7 +37,6 @@
                     <div class="error-message"></div>
 
 				<button type="submit" name="cadastrar">Cadastrar</button>
-					<button type="button" id="cancelarUpdate_btn">Cancelar</button>
 			</form>
 		</div>
     </div>
